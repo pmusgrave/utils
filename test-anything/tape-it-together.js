cf61ec6dd258a5feb025ca9b895c744eae1f6765
pmusgrave <ps.musgrave@gmail.com>
2017-05-20 12:25:17 -0400

    tape-it-together.js changed from tape ok method to tape equal method. completed.

--- test-anything/tape-it-together.js
+++ test-anything/tape-it-together.js
@@ -2,8 +2,8 @@ var fancify = require(process.argv[2]);
 var test = require('tape');
 
 test('str wrapped in fancified text', function (t) {
-  t.ok(fancify('Hello'), 'should return ~*~Hello~*~');
-  t.ok(fancify('Hello', true), 'should return ~*~HELLO~*~');
-  t.ok(fancify('Hello', false, '!'), 'should return ~!~Hello~!~');
+  t.equal(fancify('Hello'), '~*~Hello~*~');
+  t.equal(fancify('Hello', true), '~*~HELLO~*~');
+  t.equal(fancify('Hello', false, '!'), '~!~Hello~!~');
   t.end();
 })
