092d59c2d229e416b9b8e85aa050b54d24ed1dc2
pmusgrave <ps.musgrave@gmail.com>
2017-05-25 00:42:05 -0400

    modularized binary_search.js

--- algorithms/binary_search.js
+++ algorithms/binary_search.js
@@ -1,3 +1,37 @@
+module.exports = {
+  binarySearch: function (data, key) {
+      var min_index = 0;
+      var max_index = data.length;
+      var found_index = null;
+      while ((max_index - min_index) > 1) {
+          if (key == data[min_index]) {
+              found_index = min_index
+              break
+          }
+
+          if ((max_index - min_index) == 2) {
+              search_index = min_index + 1
+          }
+          else {
+              search_index = Math.floor( (max_index-min_index) / 2 ) + min_index
+          }
+          console.log("index: " + search_index);
+
+          if (key == data[search_index]) {
+              found_index = search_index
+              break
+          }
+          else if (key < data[search_index]) {
+              max_index = search_index
+          }
+          else if (key > data[search_index]) {
+              min_index = search_index
+          }
+      }
+      return found_index;
+  }
+};
+
 var random_data = new Array();
 var dataset_size = 1001;
 var i = 0
@@ -6,43 +40,16 @@ while (i <= dataset_size) {
     i += 1;
 }
 
-function binarySearch (data, key) {
-    var min_index = 0;
-    var max_index = data.length;
-    var found_index = null;
-    while ((max_index - min_index) > 1) {
-        if (key == data[min_index]) {
-            found_index = min_index
-            break
-        }
-
-        if ((max_index - min_index) == 2) {
-            search_index = min_index + 1
-        }
-        else {
-            search_index = Math.floor( (max_index-min_index) / 2 ) + min_index
-        }
-        console.log("index: " + search_index);
-
-        if (key == data[search_index]) {
-            found_index = search_index
-            break
-        }
-        else if (key < data[search_index]) {
-            max_index = search_index
-        }
-        else if (key > data[search_index]) {
-            min_index = search_index
-        }
-    }
-    return found_index;
-}
-
 var key = process.argv[2];
-var index = binarySearch(random_data, key);
-if (index != null) {
-    console.log("Value is located at index", index);
+if (!isNaN(key)) {
+  var index = module.exports.binarySearch(random_data, key);
+  if (index != null) {
+      console.log("Value is located at index", index);
+  }
+  else {
+      console.log("Value not found");
+  }
 }
 else {
-    console.log("Value not found");
+  console.log('Input was not a number');
 }
