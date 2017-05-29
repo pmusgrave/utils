082fa4c6175f4b6aeadc1d0edf62d9ebb3d65156
pmusgrave <ps.musgrave@gmail.com>
2017-05-29 10:15:57 -0400

    modularized linkedlistify, also noticed that remove node is not working properly. Adding to buglist

--- algorithms/linkedlistify.js
+++ algorithms/linkedlistify.js
@@ -1,78 +1,81 @@
 // converts an array to a singly linked list
 var arr = [0,1,2,3,4,5,6,7,8,9];
 
-var Linked_list_node = function() {
-  this.key = null;
-  this.next = null;
-}
+module.exports = {
+  Linked_list_node : function() {
+    this.key = null;
+    this.next = null;
+  },
 
-var List = function() {
-  this.head = new Linked_list_node();
-  this.tail = null;
-}
+  List : function() {
+    this.head = new module.exports.Linked_list_node();
+    this.tail = null;
+  },
 
-function linkedlistify (node, index) {
-  if (index >= arr.length - 1) {
-    node.key = arr[index];
-    node.next = null;
-    L.tail = node;
-  }
-  else {
-    node.key = arr[index];
-    node.next = linkedlistify(new Linked_list_node(), index + 1);
-  }
-  return node;
-}
-
-function traverseList (node) {
-  if(node.next == null) {
-    console.log(node.key);
-    return;
-  }
-  else {
-    console.log(node.key);
-    traverseList(node.next);
-  }
-}
-
-function searchList(node, searchKey) {
-  if (node.key == searchKey) {
-    console.log("Key found");
-    console.log(node);
+  linkedlistify : function (node, index) {
+    if (index >= arr.length - 1) {
+      node.key = arr[index];
+      node.next = null;
+      L.tail = node;
+    }
+    else {
+      node.key = arr[index];
+      node.next = this.linkedlistify(new module.exports.Linked_list_node(), index + 1);
+    }
     return node;
-  }
-  else if (node.next == null){
-    console.log("Key not found");
-    return null;
-  }
-  else {
-    searchList(node.next, searchKey);
-  }
-}
+  },
 
-function insertNode(newNode,tail) {
-  tail.next = newNode;
-  tail = newNode;
-}
-
-function removeNode(node, node_to_remove) {
-  if (node != null) {
-    if(node.next === node_to_remove) {
-      node.next = node.next.next;
+  traverseList : function (node) {
+    if(node.next == null) {
+      console.log(node.key);
       return;
     }
     else {
-      removeNode(node.next, node_to_remove);
-      return;
+      console.log(node.key);
+      this.traverseList(node.next);
     }
-  }
+  },
+
+  searchList : function (node, searchKey) {
+    if (node.key == searchKey) {
+      console.log("Key found");
+      console.log(node);
+      return node;
+    }
+    else if (node.next == null){
+      console.log("Key not found");
+      return null;
+    }
+    else {
+      this.searchList(node.next, searchKey);
+    }
+  },
+
+  insertNode : function (newNode,tail) {
+    tail.next = newNode;
+    tail = newNode;
+  },
+
+  removeNode : function(node, node_to_remove) {
+    if (node != null) {
+      if(node.next === node_to_remove) {
+        node.next = node.next.next;
+        return;
+      }
+      else {
+        this.removeNode(node.next, node_to_remove);
+        return;
+      }
+    }
+  },
 }
 
-var L = new List();
+
+var L = new module.exports.List();
 L.head.next = L.tail;
-linkedlistify(L.head, 0);
-traverseList(L.head);
-var node_to_remove = searchList(L.head, 4);
+module.exports.linkedlistify(L.head, 0);
+module.exports.traverseList(L.head);
+var node_to_remove = module.exports.searchList(L.head, 4);
 console.log(node_to_remove);
-removeNode(L.head, node_to_remove);
-traverseList(L.head);
+module.exports.removeNode(L.head, node_to_remove);
+module.exports.traverseList(L.head);
