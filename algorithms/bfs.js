91be53e66c651b63ebaa26804464754fd0740842
pmusgrave <ps.musgrave@gmail.com>
2017-06-01 20:58:49 -0400

    modularized bfs

--- algorithms/bfs.js
+++ algorithms/bfs.js
@@ -23,46 +23,49 @@ graph.push(new vertex(7,[3,6]));
 // a search algorithm to find the graph object from a given key.
 // Vertices will be sorted by their keys to make searching fast.
 
-function findGraphObject(graph, key) {
-  var min_index = 0;
-  var max_index = graph.length;
-  var found_index = null;
-  while ((max_index - min_index) > 1) {
-      if (key == graph[min_index].key) {
-          found_index = min_index
-          break
-      }
+module.exports = {
+  findGraphObject : function (graph, key) {
+    // graph argument should be an adjacency list
+    var min_index = 0;
+    var max_index = graph.length;
+    var found_index = null;
+    while ((max_index - min_index) > 1) {
+        if (key == graph[min_index].key) {
+            found_index = min_index
+            break
+        }
 
-      if ((max_index - min_index) == 2) {
-          search_index = min_index + 1
-      }
-      else {
-          search_index = Math.floor( (max_index-min_index) / 2 ) + min_index
-      }
-      //console.log("index: " + search_index);
+        if ((max_index - min_index) == 2) {
+            search_index = min_index + 1
+        }
+        else {
+            search_index = Math.floor( (max_index-min_index) / 2 ) + min_index
+        }
+        //console.log("index: " + search_index);
 
-      if (key == graph[search_index].key) {
-          found_index = search_index
-          break
-      }
-      else if (key < graph[search_index].key) {
-          max_index = search_index
-      }
-      else if (key > graph[search_index].key) {
-          min_index = search_index
-      }
+        if (key == graph[search_index].key) {
+            found_index = search_index
+            break
+        }
+        else if (key < graph[search_index].key) {
+            max_index = search_index
+        }
+        else if (key > graph[search_index].key) {
+            min_index = search_index
+        }
+    }
+    return found_index;
   }
-  return found_index;
 }
 
 var queue = [];
 var starting_position = 0;
-queue.push(graph[findGraphObject(graph, starting_position)]);
+queue.push(graph[module.exports.findGraphObject(graph, starting_position)]);
 queue[0].distance = 0;
 
 while (queue.length > 0) {
   for (var i = 0; i < queue[0].adj.length; i++) {
-    var nextAdj = graph[findGraphObject(graph, queue[0].adj[i])];
+    var nextAdj = graph[module.exports.findGraphObject(graph, queue[0].adj[i])];
     if (nextAdj.color == "white") {
       queue.push(nextAdj);
       nextAdj.color = "grey";
