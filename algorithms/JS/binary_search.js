f49f1dbcfd233f3ac3372a7a7dc792a41aa98777
pmusgrave <ps.musgrave@gmail.com>
2017-08-19 12:12:42 -0400

    refactored binary_search.js

--- algorithms/JS/binary_search.js
+++ algorithms/JS/binary_search.js
@@ -1,30 +1,24 @@
 module.exports = {
   binarySearch: function (data, key) {
+      var search_index = 0;
       var min_index = 0;
       var max_index = data.length;
       var found_index = null;
       while ((max_index - min_index) > 1) {
-          if (key == data[min_index]) {
+          search_index = getNextSearchIndex(min_index, max_index);
+
+          if (data[min_index] == key ) {
               found_index = min_index
               break
           }
-
-          if ((max_index - min_index) == 2) {
-              search_index = min_index + 1
-          }
-          else {
-              search_index = Math.floor( (max_index-min_index) / 2 ) + min_index
-          }
-          console.log("index: " + search_index);
-
-          if (key == data[search_index]) {
+          else if (data[search_index] == key) {
               found_index = search_index
               break
           }
-          else if (key < data[search_index]) {
+          else if (data[search_index] > key) {
               max_index = search_index
           }
-          else if (key > data[search_index]) {
+          else if (data[search_index] < key) {
               min_index = search_index
           }
       }
@@ -32,6 +26,19 @@ module.exports = {
   }
 };
 
+function getNextSearchIndex(min_index, max_index) {
+    var search_index = 0;
+    if ((max_index - min_index) == 2) {
+        search_index = min_index + 1
+    }
+    else {
+        search_index = Math.floor( (max_index-min_index) / 2 ) + min_index
+    }
+    console.log("index: " + search_index);
+    return search_index;
+}
+
+// testing
 var random_data = new Array();
 var dataset_size = 1001;
 var i = 0
