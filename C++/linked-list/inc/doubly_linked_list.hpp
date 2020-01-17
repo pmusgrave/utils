efa8de495cb8516e84a3308d8f9236d122a880fe
Paul Musgrave <ps.musgrave@gmail.com>
2020-01-17 02:49:07 -0500

    add better unit testing framework for C++.
    On other machines, I used the Catch unit testing framework and installed it in /usr/local/include

--- C++/linked-list/inc/doubly_linked_list.hpp
+++ C++/linked-list/inc/doubly_linked_list.hpp
@@ -9,10 +9,19 @@ template<typename T>
 
 class DoublyLinkedList {
 public:
+  Node<T>* head;
+
+  DoublyLinkedList() {
+    this->head = nullptr;
+  }
+  DoublyLinkedList(T val) {
+    this->head = new Node<T>(val);
+  }
   DoublyLinkedList(Node<T> *head) {
     this->head = head;
   };
   ~DoublyLinkedList() {
+    if(this->head == nullptr) { return; }
     Node<T>* current_node = this->head->next;
     while(current_node->next != nullptr){
       if(current_node->prev != nullptr){
@@ -24,19 +33,35 @@ public:
       }
     }
     //printf("Freeing %d\n", current_node->prev->val);
-    delete current_node->prev;
+    if(current_node->prev != nullptr) {
+      delete current_node->prev;
+    }
     delete current_node;
   };
-  Node<T>* head;
 
-  void insert(T val, Node<T> *head) {
-    Node<T> *current_node = head;
-    while (current_node->next != NULL) {
-      current_node = current_node->next;
+  void insert(T val, Node<T> *location) {
+    if(location == nullptr && this->head == nullptr) {
+      Node<T> *new_node = new Node<T>(val);
+      this->head->prev = new_node;
+      new_node->next = this->head;
+      this->head = new_node;
     }
-    Node<T> *new_node = new Node<T>(val);
-    current_node->next = new_node;
-    new_node->prev = current_node;
+    else if (location == nullptr && this->head != nullptr) {
+      Node<T> *current_node = location;
+      while (current_node->next != NULL) {
+        current_node = current_node->next;
+      }
+      Node<T> *new_node = new Node<T>(val);
+      current_node->next = new_node;
+      new_node->prev = current_node;
+    }
+    else {
+      Node<T> *new_node = new Node<T>(val);
+      Node<T> *temp = location->next;
+      location->next = new_node;
+      new_node->prev = location;
+      temp->prev = new_node;
+    }
   }
 
   Node<T> *delete_node(Node<T> *node) {
--- C++/linked-list/test/test_doubly_linked_list.cpp
+++ C++/linked-list/test/test_doubly_linked_list.cpp
@@ -1,9 +1,43 @@
-#include <assert.h>
-#include <stdio.h>
+#define CATCH_CONFIG_MAIN
+#include <catch.hpp>
 #include "doubly_linked_list.hpp"
 #include "node.hpp"
 
-int main(){
+TEST_CASE("Creating new nodes", "[node_constructor]") {
+  Node<int> *int_node = new Node<int>(0);
+  REQUIRE(int_node->val == 0);
+  REQUIRE(int_node->next == nullptr);
+  REQUIRE(int_node->prev == nullptr);
+
+  Node<char> *char_node = new Node<char>('0');
+  REQUIRE(char_node->val == '0');
+  REQUIRE(char_node->next == nullptr);
+  REQUIRE(char_node->prev == nullptr);
+
+  delete int_node;
+  delete char_node;
+}
+
+TEST_CASE("Creating a new linked list", "[]") {
+  DoublyLinkedList<int> *list = new DoublyLinkedList<int>;
+  REQUIRE(list->head == nullptr);
+  delete list;
+
+  DoublyLinkedList<char> *char_list = new DoublyLinkedList<char>;
+  REQUIRE(char_list->head == nullptr);
+  delete char_list;
+
+  // Node<char> *char_node = new Node<char>('q');
+  // DoublyLinkedList<char> char_list_two(char_node);
+  // REQUIRE(char_list2.head != nullptr);
+  //  REQUIRE(char_list2.head->val == 'q');
+}
+
+//TEST_CASE("Inserting nodes", "[insert]") {
+
+//}
+
+/*int main(){
   Node<char> *head = new Node<char>('a');
   DoublyLinkedList<char> list(head);
   char arr[10] = {'u','q','b','d','f','i','e','z','n','b'};
@@ -28,3 +62,4 @@ int main(){
 
   return 0;
 }
+*/
