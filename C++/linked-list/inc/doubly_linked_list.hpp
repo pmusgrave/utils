8adb62d79379984df200094f1abfa6e55516d100
pmusgrave <ps.musgrave@gmail.com>
2020-01-17 13:50:25 -0500

    add unit tests for inserting linked list nodes

--- C++/linked-list/inc/doubly_linked_list.hpp
+++ C++/linked-list/inc/doubly_linked_list.hpp
@@ -13,24 +13,28 @@ public:
 
   DoublyLinkedList() {
     this->head = nullptr;
+    std::cout << "constructor 1 " << this->head << std::endl;
   }
   DoublyLinkedList(T val) {
+    std::cout << "constructor 2" << val << std::endl;
     this->head = new Node<T>(val);
   }
   DoublyLinkedList(Node<T> *head) {
     this->head = head;
+    std::cout << "constructor 3 " << this->head->val << std::endl;
   };
   ~DoublyLinkedList() {
     if(this->head == nullptr) { return; }
-    Node<T>* current_node = this->head->next;
+    std::cout << "destructor " << this->head->val << std::endl;
+    Node<T> *current_node = this->head;
     while(current_node->next != nullptr){
       if(current_node->prev != nullptr){
         // printf("Current node is %d\n", current_node->val);
         // printf("Freeing %d\n", current_node->prev->val);
         // printf("Next node is %d\n", current_node->next->val);
         delete current_node->prev;
-        current_node = current_node->next;
       }
+      current_node = current_node->next;
     }
     //printf("Freeing %d\n", current_node->prev->val);
     if(current_node->prev != nullptr) {
@@ -39,27 +43,28 @@ public:
     delete current_node;
   };
 
+  // inserts new node with value val immediately following location
+  // if location is a null pointer, insert node at end of list
   void insert(T val, Node<T> *location) {
     if(location == nullptr && this->head == nullptr) {
       Node<T> *new_node = new Node<T>(val);
-      this->head->prev = new_node;
-      new_node->next = this->head;
       this->head = new_node;
     }
     else if (location == nullptr && this->head != nullptr) {
-      Node<T> *current_node = location;
-      while (current_node->next != NULL) {
+      Node<T> *current_node = this->head;
+      while (current_node->next != nullptr) {
         current_node = current_node->next;
       }
       Node<T> *new_node = new Node<T>(val);
       current_node->next = new_node;
       new_node->prev = current_node;
     }
-    else {
+    else if(location != nullptr && this->head != nullptr){
       Node<T> *new_node = new Node<T>(val);
       Node<T> *temp = location->next;
       location->next = new_node;
       new_node->prev = location;
+      new_node->next = temp;
       temp->prev = new_node;
     }
   }
@@ -84,18 +89,18 @@ public:
     return this->head;
   }
 
-  bool find_val(T val, Node<T> *root) {
+  Node<T>* find_val(T val) {
     Node<T> *current_node = head;
     while (current_node != nullptr){
       if (current_node->val == val){
-        return true;
+        return current_node;
       }
     }
-    return false;
+    return nullptr;
   }
 
-  void print_linked_list(Node<T> *root) {
-    Node<T> *current_node = head;
+  void print_linked_list() {
+    Node<T> *current_node = this->head;
     while(current_node != nullptr){
       std::cout << current_node->val << std::endl;
       current_node = current_node->next;
--- C++/linked-list/test/test_doubly_linked_list.cpp
+++ C++/linked-list/test/test_doubly_linked_list.cpp
@@ -1,5 +1,6 @@
 #define CATCH_CONFIG_MAIN
 #include <catch.hpp>
+#include <iostream>
 #include "doubly_linked_list.hpp"
 #include "node.hpp"
 
@@ -27,10 +28,35 @@ TEST_CASE("Creating a new linked list", "[]") {
   REQUIRE(char_list->head == nullptr);
   delete char_list;
 
-  // Node<char> *char_node = new Node<char>('q');
-  // DoublyLinkedList<char> char_list_two(char_node);
-  // REQUIRE(char_list2.head != nullptr);
-  //  REQUIRE(char_list2.head->val == 'q');
+  Node<char> *char_node = new Node<char>('q');
+  DoublyLinkedList<char> char_list_two(char_node);
+  REQUIRE(char_list_two.head != nullptr);
+  REQUIRE(char_list_two.head->val == 'q');
+  REQUIRE(char_list_two.head->next == nullptr);
+}
+
+TEST_CASE("Inserting nodes into list", "[]"){
+  DoublyLinkedList<char> *null_initialized_list = new DoublyLinkedList<char>;
+  REQUIRE(null_initialized_list->head == nullptr);
+  null_initialized_list->insert('a', nullptr);
+  REQUIRE(null_initialized_list->head != nullptr);
+  REQUIRE(null_initialized_list->head->val == 'a');
+  REQUIRE(null_initialized_list->head->prev == nullptr);
+  REQUIRE(null_initialized_list->head->next == nullptr);
+
+  null_initialized_list->insert('b',nullptr);
+  REQUIRE(null_initialized_list->head != nullptr);
+  REQUIRE(null_initialized_list->head->val == 'a');
+  REQUIRE(null_initialized_list->head->next->val == 'b');
+
+  //  null_initialized_list->print_linked_list();
+  Node<char> *target = null_initialized_list->head;
+  REQUIRE(target->val == 'a');
+  null_initialized_list->insert('c',target);
+  REQUIRE(null_initialized_list->head->val == 'a');
+  REQUIRE(null_initialized_list->head->next->val == 'c');
+  REQUIRE(null_initialized_list->head->next->next->val == 'b');
+  null_initialized_list->print_linked_list();
 }
 
 //TEST_CASE("Inserting nodes", "[insert]") {
