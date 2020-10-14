83453d2a1b30aba63474ac86e1d51d6ac6788a8e
pmusgrave <ps.musgrave@gmail.com>
2020-10-14 15:55:25 -0400

    add defines to instrumentor to enable/disable profiling
    Add preprocessor directive to enable overall profiling.
    Add preprocessor macro to label the profiling output with the function
    name and line number of the function being profiled.

--- C++/benchmarking/inc/instrumentation_timer.hpp
+++ C++/benchmarking/inc/instrumentation_timer.hpp
@@ -1,17 +1,22 @@
+#ifndef INC_INSTRUMENTATION_TIMER_HPP_
+#define INC_INSTRUMENTATION_TIMER_HPP_
+
 // source: https://youtu.be/YG4jexlSAjc?list=PLlrATfBNZ98dudnM48yfGUldqGD0S4FFb
+
 #include <chrono>
 #include <iostream>
 #include <memory>
+#include <thread>
 
 #include "instrumentor.hpp"
 
-class Timer {
+class InstrumentationTimer {
 public:
-  Timer(const char* name) : name(name) {
+  InstrumentationTimer(const char* name) : name(name) {
     start_time_point = std::chrono::high_resolution_clock::now();
   }
 
-  ~Timer() {
+  ~InstrumentationTimer() {
     stop();
   }
 
@@ -19,10 +24,11 @@ public:
     auto end_time_point = std::chrono::high_resolution_clock::now();
     auto start = std::chrono::time_point_cast<std::chrono::microseconds>(start_time_point).time_since_epoch().count();
     auto end = std::chrono::time_point_cast<std::chrono::microseconds>(end_time_point).time_since_epoch().count();
-    auto duration = end - start;
-    double ms = duration * 0.001;
+    //auto duration = end - start;
+    //double ms = duration * 0.001;
     //std::cout << name << " : " << duration << "us (" << ms << "ms)" << std::endl;
-    Instrumentor::get().write_profile({ name, start, end });
+    uint32_t thread_id = std::hash<std::thread::id>{}(std::this_thread::get_id());
+    Instrumentor::get().write_profile({ name, start, end, thread_id });
   }
 
 private:
@@ -30,5 +36,4 @@ private:
   std::chrono::time_point<std::chrono::high_resolution_clock> start_time_point;
 };
 
-
-#endif  // UTILS_CPP_INC_INSTRUMENTOR_HPP_
+#endif // INC_INSTRUMENTATION_TIMER_HPP_
--- C++/benchmarking/inc/instrumentor.hpp
+++ C++/benchmarking/inc/instrumentor.hpp
@@ -1,9 +1,18 @@
-#ifndef UTILS_CPP_INC_INSTRUMENTOR_HPP_
-#define UTILS_CPP_INC_INSTRUMENTOR_HPP_
+#ifndef INC_INSTRUMENTOR_HPP_
+#define INC_INSTRUMENTOR_HPP_
 
 // source: https://youtu.be/xlAH4dbMVnU?list=PLlrATfBNZ98dudnM48yfGUldqGD0S4FFb
 // This outputs timing data from the Timer class in a JSON format readable by
 // Google Chrome tracing
+
+#define PROFILING 0
+#if(PROFILING)
+#define PROFILE(name) InstrumentationTimer timer##__LINE__(name)
+#define PROFILE_FUNCTION() PROFILE(__PRETTY_FUNCTION__)
+#else
+#define PROFILE(name)
+#define PROFILE_FUNCTION()
+#endif
 
 #include <algorithm>
 #include <chrono>
@@ -39,7 +48,7 @@ class Instrumentor {
   }
 
   void write_profile(const ProfileResult& result) {
-    if (profile_count++ > 0)
+    if (profile_count++ > 3)  // not sure why 0 doesn't work here
       output_stream << ",";
 
     std::string name = result.name;
@@ -79,37 +88,4 @@ class Instrumentor {
   int profile_count;
 };
 
-// source: https://youtu.be/YG4jexlSAjc?list=PLlrATfBNZ98dudnM48yfGUldqGD0S4FFb
-#include <chrono>
-#include <iostream>
-#include <memory>
-
-#include "instrumentor.hpp"
-
-class Timer {
-public:
-  Timer(const char* name) : name(name) {
-    start_time_point = std::chrono::high_resolution_clock::now();
-  }
-
-  ~Timer() {
-    stop();
-  }
-
-  void stop() {
-    auto end_time_point = std::chrono::high_resolution_clock::now();
-    auto start = std::chrono::time_point_cast<std::chrono::microseconds>(start_time_point).time_since_epoch().count();
-    auto end = std::chrono::time_point_cast<std::chrono::microseconds>(end_time_point).time_since_epoch().count();
-    auto duration = end - start;
-    double ms = duration * 0.001;
-    //std::cout << name << " : " << duration << "us (" << ms << "ms)" << std::endl;
-    Instrumentor::get().write_profile({ name, start, end });
-  }
-
-private:
-  const char* name;
-  std::chrono::time_point<std::chrono::high_resolution_clock> start_time_point;
-};
-
-
-#endif  // UTILS_CPP_INC_INSTRUMENTOR_HPP_
+#endif // INC_INSTRUMENTOR_HPP_
