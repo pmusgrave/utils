b2936ba00376f905edf7160b671a4d73a0054e0c
pmusgrave <ps.musgrave@gmail.com>
2021-06-10 16:58:08 -0400

    Add branch again (undoes a77c6dc)
    I like it better when you explicitly check for objects, because
    otherwise string arguments are printed with quote characters, which
    looks worse to me.

--- JS/convenience/debug.js
+++ JS/convenience/debug.js
@@ -1,6 +1,10 @@
 function dlog(str) {
     if (process.env.DEBUG) {
-        console.log(`${(new Date).toISOString()}: \n${JSON.stringify(str, null, "  ")}`);
+        if (typeof str === 'object' && str !== null) {
+            console.log(`${(new Date).toISOString()}: \n${JSON.stringify(str, null, "  ")}`);
+        } else {
+            console.log(`${(new Date).toISOString()}: ${str}`);
+        }
     }
 }
 
